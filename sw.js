440aa6abbd6c9612e72488c536bafedad96204e6
Ronaldo Alcantara <r13araujo@gmail.com>
2026-04-14 09:02:20 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,20 @@
-const CACHE_NAME = 'alcantara-games-v2';
-const APP_SHELL = [
-  './',
-  './index.html',
-  './manifest.json',
-  './sw.js',
-  './logo.png',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+const CACHE_NAME = "alcantara-games-ultra-master-final-v1";
+const APP_ASSETS = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS)));
   self.skipWaiting();
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-  );
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', event => {
-  const req = event.request;
-  if (req.method !== 'GET') return;
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
   event.respondWith(
-    caches.match(req).then(cached => cached || fetch(req).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(() => {});
-      return res;
-    }).catch(() => caches.match('./index.html')))
+    caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+      const cloned = response.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(event.request, cloned)).catch(() => {});
+      return response;
+    }).catch(() => caches.match("./index.html")))
   );
 });
